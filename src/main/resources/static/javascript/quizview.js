b0bd2fe7294e9a04088da038a95bc7d567b70b1d
selloriwoo <sellpriwoo@gmail.com>
2023-06-18 16:28:55 +0900

    delete js

--- src/main/resources/static/javascript/quizview.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('dialog-opener').addEventListener('click', function() {
-    document.getElementById('dialog').showModal()
-})
-
-document.getElementById('dialog-closer').addEventListener('click', function() {
-    document.getElementById('dialog').close()
-})
